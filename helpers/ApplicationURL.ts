6c944175b154c05b575492fb12784183c0a73d4a
Moran Liv <moran.liv2006@gmail.com>
2025-12-09 03:04:09 +0200

    Update the ApplicationURL with more URLs.

--- helpers/ApplicationURL.ts
+++ helpers/ApplicationURL.ts
@@ -1,3 +1,6 @@
 export default class ApplicationURL {
-    public static readonly HOME_PAGE_ULR = "https://atid.store/store/";
+    public static readonly BASE_ULR = "https://atid.store/";
+    public static readonly STORE_ULR = `${this.BASE_ULR}store/`;
+    public static readonly CART_ULR = `${this.BASE_ULR}cart-2/`;
+    public static readonly CHECKOUT_ULR = `${this.BASE_ULR}checkout-2/`;
 }
